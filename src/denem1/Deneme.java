6a7b10832ad9344801ba8cb7876f359805717868
servetbilge <servetbilgebay@gmail.com>
2020-07-07 15:35:33 +0200

    İlk deneme01 e dosya gönderme

---
+++ src/denem1/Deneme.java
@@ -0,0 +1,11 @@
+package denem1;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lokal master repo dan ilk commit");
+
+	}
+
+}
---
+++ src/denem1/package-info.java
@@ -0,0 +1,1 @@
+package denem1;
