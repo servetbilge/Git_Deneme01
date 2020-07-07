a76fc3c6bd5552cf495fa561ad57280806446909
servetbilge <67020144+servetbilge@users.noreply.github.com>
2020-07-07 15:38:14 +0200

    Update Deneme.java
    repo master dan ilk commit yazdım

--- src/denem1/Deneme.java
+++ src/denem1/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		
 		System.out.println("Lokal master repo dan ilk commit");
+		System.out.println(" master repo dan ilk commit");
 
 	}
 
